2480c05e42cd3cd59b7f32e8ba38b39f4d7c1c84
Dimitar Dragostinov <dimitar.dragostinov@gmail.com>
2018-08-14 16:03:23 +0300

    fix removeNotRequiredBracesFrom to handle empty strings

--- test/app/controller/Root.js
+++ test/app/controller/Root.js
@@ -9,6 +9,16 @@ Ext
         myOtherMixin: 'My.mixin.MyOtherMixin'
     },
 
+    _getCommonActions: function () {
+        var result = [
+            { popular: true, iconCls: '', text: 'A text', config: { type: 'text' } },
+            { popular: true, iconCls: 'icon-generic-newitem', text: 'New item menu', config: { type: 'newitem' } },
+            { popular: true, iconCls: '', text: 'Separator', config: { type: 'separator' } },
+            { popular: true, iconCls: 'icon-arrows-right', text: 'Right align', config: { type: 'right' } },
+            { popular: false, iconCls: 'icon-arrows-down', text: 'Menu list', config: { type: 'menulist' } },
+        ];
+        return result;
+    },
  
     isOdd: function(number) {
         return number % 2 === 1;
